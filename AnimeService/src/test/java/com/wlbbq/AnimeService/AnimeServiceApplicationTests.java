1cbd901f34d1c4723cf8f6cd4448aa88f11b0336
Gonzalo Mora <gonzmoca@gmail.com>
2024-11-08 09:09:03 +0100

    Esqueleto proyecto

---
+++ AnimeService/src/test/java/com/wlbbq/AnimeService/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlbbq.AnimeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
